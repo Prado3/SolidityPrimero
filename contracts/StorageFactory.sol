927391c8c687954621ac8a3cd02a49ffcbfd484c
Prado3 <lukasprado8@gmail.com>
2025-03-11 15:06:31 -0300

    final

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {SimpleStorage} from "./SimpleStorage.sol";
+
 contract StorageFactory {
-    
+    SimpleStorage[] public listOfSimpleStorageContract;
+
+    function createSimpleStorageContract () public {
+        SimpleStorage newlistOfSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContract.push(newlistOfSimpleStorageContract);
+    }
+
+    function stStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function stGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContract[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
+    }
 }
